43ee33d8e80b93e1d965866f5a2ccac0ef46af9f
Sylvia Xie <100256628+SYLVIAXIE1997@users.noreply.github.com>
2022-12-21 22:56:16 +1100

    feat: sidebar component completed

--- src/index.js
+++ src/index.js
@@ -2,7 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import {ContextProvider} from "./contexts/ContextProvider";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(
+    <ContextProvider>
+        <App />
+    </ContextProvider>
+);
 
