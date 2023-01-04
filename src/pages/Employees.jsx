743db49230928e9532c21056db733bb7f1767ca1
Sylvia Xie <100256628+SYLVIAXIE1997@users.noreply.github.com>
2023-01-04 16:22:20 +1100

    feat: completed employees page

--- src/pages/Employees.jsx
+++ src/pages/Employees.jsx
@@ -1,9 +1,27 @@
 import React from 'react';
+import {ColumnDirective, ColumnsDirective, GridComponent, Page, Search, Toolbar, Inject} from "@syncfusion/ej2-react-grids";
+
+import {employeesData, employeesGrid} from "../data/dummy";
+import {Header} from "../components";
 
 const Employees = () => {
     return (
-        <div>
-
+        <div className="m-2 md:m-10 p-2 md:p-10 bg-white rounded-3xl">
+            <Header category="page" title="Employees"/>
+            <GridComponent
+                dataSource={employeesData}
+                allowPaging
+                allowSorting
+                toolbar={['Search']}
+                width="auto"
+            >
+                <ColumnsDirective>
+                    {employeesGrid.map((item, index)=>(
+                        <ColumnDirective key={index} {...item}/>
+                    ))}
+                </ColumnsDirective>
+                <Inject services={[Page,Search, Toolbar]}/>
+            </GridComponent>
         </div>
     );
 };
